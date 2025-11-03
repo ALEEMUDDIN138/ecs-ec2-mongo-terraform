e73f0270a62f616db9e1103f08bb0bd958526f09
ALEEMUDDIN138 <aleemuddin138@gmail.com>
2025-11-03 10:20:42 +0530

    Update outputs.tf

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,24 +1,15 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
-}
-
 output "public_subnet_ids" {
-  description = "IDs of public subnets"
-  value       = aws_subnet.public[*].id
+  value = aws_subnet.public[*].id
 }
 
 output "private_subnet_ids" {
-  description = "IDs of private subnets"
-  value       = aws_subnet.private[*].id
-}
-
-output "alb_security_group_id" {
-  description = "ID of ALB security group"
-  value       = aws_security_group.alb.id
+  value = aws_subnet.private[*].id
 }
 
 output "ecs_security_group_id" {
-  description = "ID of ECS security group"
-  value       = aws_security_group.ecs.id
+  value = aws_security_group.ecs.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
